6e6b9988b4f972b134c2dc369ac1f66ae276c12c
AkshatJha21 <akshatj2134@gmail.com>
2024-09-20 17:22:02 +0530

    signup structure added

---
+++ app/signup/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <div className='h-screen flex items-center justify-center w-full'>
+        <div className='h-[90vh] rounded opacity-55 bg-[#F2F2F2] text-[#121417] w-[30%] mx-10'>
+            Carousel
+        </div>
+        <div className='w-[70%]'>
+            <div className='w-full flex justify-center'>
+                Form
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Signup   
