2b04ab8ae06efb7a8fd5767e4ed2e1eac0ffdfb0
AkshatJha21 <akshatj2134@gmail.com>
2024-09-22 18:08:31 +0530

    POST route done for signup

--- app/api/user/signup/route.ts
+++ app/api/user/signup/route.ts
@@ -1,8 +1,62 @@
-// install prisma DONE
-// write user schema DONE
-// add zod type validation 
-// create POST route
-// test on postman
-// hash password using bcrypt
-// store in Postgres
-// 
+import { signupInput } from "@/helpers/zod";
+import { db } from "@/lib/db";
+import { NextRequest, NextResponse } from "next/server";
+import { genSalt, hash } from "bcrypt-ts";
+import { sign } from "jsonwebtoken";
+import { JWT_SECRET } from "@/helpers/constants";
+
+export async function POST (request: NextRequest) {
+    try {
+        const reqBody = await request.json();
+        const parsedBody = signupInput.safeParse(reqBody);
+
+        if (!parsedBody.success) {
+            return NextResponse.json({
+                error: "Incorrect inputs"
+            }, {
+                status: 400
+            });
+        }
+
+        console.log(reqBody);
+        
+        const existingUser = await db.user.findUnique({
+            where: {
+                email: reqBody.email
+            }
+        });
+
+        if (existingUser) {
+            return NextResponse.json({
+                error: "User already exists"
+            }, {
+                status: 400
+            });
+        }
+
+        const salt = await genSalt(10);
+        const hashedPassword = await hash(reqBody.password, salt);
+
+        const newUser = await db.user.create({
+            data: {
+                email: reqBody.email,
+                password: hashedPassword,
+                role: "Student"
+            }
+        });
+
+        const token = await sign({id: newUser.id}, JWT_SECRET);
+
+        return NextResponse.json({
+            message: "User created successfully",
+            jwt: token
+        });
+
+    } catch (error: any) {
+        return NextResponse.json({
+            error: error
+        }, {
+            status: 500
+        });
+    }
+}
