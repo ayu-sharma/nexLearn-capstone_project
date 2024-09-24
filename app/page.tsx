e173dcc9884dd0ee324695641f2069faee94edee
AkshatJha21 <akshatj2134@gmail.com>
2024-09-25 00:18:49 +0530

    build landing page

--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,13 @@ export default function Home() {
 
   const router = useRouter();
 
-  useEffect(() => {
-    router.push('/login');
-  }, []);
+  // useEffect(() => {
+  //   router.push('/login');
+  // }, []);
 
   return (
     <div className="">
-      
+      // Landing Page here
     </div>
   );
 }
