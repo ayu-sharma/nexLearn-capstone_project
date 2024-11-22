ad3c824d87e0ab41ba97fc177aee5da3014d0628
AkshatJha21 <akshatj2134@gmail.com>
2024-11-22 12:12:19 +0530

    grid skeleton added

--- components/custom/library/library-grid.tsx
+++ components/custom/library/library-grid.tsx
@@ -11,6 +11,7 @@ interface LibraryGridProps {
 
 const LibraryGrid = ({ searchTerm, filter }: LibraryGridProps) => {
     const [courses, setCourses] = useState<any[]>([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const fetchCourses = async () => {
@@ -18,7 +19,9 @@ const LibraryGrid = ({ searchTerm, filter }: LibraryGridProps) => {
             const response = await axios.get('http://localhost:3000/api/courses')
             setCourses(response.data.courses);
           } catch (error) {
-            console.error('Error fetching courses:', error)
+            console.error('Error fetching courses:', error);
+          } finally {
+            setLoading(true);
           }
         }
     
@@ -32,9 +35,12 @@ const LibraryGrid = ({ searchTerm, filter }: LibraryGridProps) => {
     })
   return (
     <div className='grid grid-cols-4 gap-6'>
-        {filteredCourses.map((course) => (
-        <CourseCard key={course.id} title={course.title} description={course.description} type={course.type} />
-      ))}
+        {loading 
+            ? Array.from({ length: 8 }).map((_, index) => <SkeletonCard key={index} />)
+            : filteredCourses.map((course) => (
+                <CourseCard key={course.id} title={course.title} description={course.description} type={course.type} />
+              ))
+        }
     </div>
   )
 }
@@ -74,5 +80,21 @@ const CourseCard = ({ title, description, type }: CourseCardProps) => {
                 <ArrowRight />
             </div>
         </div>
+    )
+}
+
+const SkeletonCard = () => {
+    return (
+        <div className='animate-pulse shadow-sm p-6 border rounded-md flex flex-col justify-between gap-y-4'>
+        <div className='flex flex-col gap-y-4'>
+          <div className='flex items-center gap-x-2'>
+            <div className='w-8 h-8 bg-gray-300/20 rounded-full'></div>
+            <div className='w-2/3 h-6 bg-gray-300/20 rounded'></div>
+          </div>
+          <div className='w-full h-4 bg-gray-300/20 rounded'></div>
+          <div className='w-5/6 h-4 bg-gray-300/20 rounded'></div>
+        </div>
+        <div className='w-1/3 h-6 bg-gray-300/20 rounded'></div>
+      </div>        
     )
 }
