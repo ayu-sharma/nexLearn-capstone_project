0df4a10c5cfa68e80a42ca436728b347f0b5de58
AkshatJha21 <akshatj2134@gmail.com>
2024-11-23 17:13:40 +0530

    layout

--- components/custom/course/course-view.tsx
+++ components/custom/course/course-view.tsx
@@ -1,9 +1,10 @@
 "use client";
 
+import { Button } from '@/components/ui/button';
 import { Checkbox } from '@/components/ui/checkbox';
 import axios from 'axios';
-import { BookA, Code, DraftingCompass, File, Video } from 'lucide-react';
-import React, { useEffect, useState } from 'react'
+import { BookA, ChevronRight, Code, DraftingCompass, File, Video } from 'lucide-react';
+import React, { useEffect, useState } from 'react';
 
 interface CourseViewProps {
   courseId: string | null;
@@ -73,23 +74,36 @@ const CourseView = ({ courseId }: CourseViewProps) => {
         </div>
         <p className='font-light'>{course.description}</p>
       </div>
-      <div className="grid grid-cols-1 gap-6 w-1/4">
-        {course.modules.map((module) => (
-          <div
-            key={module.id}
-            className="flex justify-between items-center p-4 border rounded transition cursor-pointer hover:border-neutral-400"
-          >
-            <div className='flex flex-col gap-2'>
-              <div className='flex items-center'>
-                <File className='h-5 mr-2' />
-                <h2>Module {module.id}</h2>
+      <div className='flex gap-6 h-[75vh]'>
+        <div className="flex flex-col gap-6 w-1/4">
+          {course.modules.map((module) => (
+            <div
+              key={module.id}
+              className="flex justify-between items-center p-4 border rounded transition cursor-pointer hover:border-neutral-400"
+            >
+              <div className='flex flex-col gap-2'>
+                <div className='flex items-center'>
+                  <File className='h-5 mr-2' />
+                  <h2>Module {module.id}</h2>
+                </div>
+                <h3 className="text-xl font-semibold">{module.title}</h3>
               </div>
-              <h3 className="text-xl font-semibold">{module.title}</h3>
+              <Checkbox />
+              {/* <Video /> */}
             </div>
-            <Checkbox />
-            {/* <Video /> */}
+          ))}
+        </div>
+        <div className='relative h-full w-3/4'>
+          <h1>Reading Material</h1>
+          <div className='absolute bottom-0 flex w-full justify-end gap-x-4'>
+            <Button>
+              Mark as Completed
+            </Button>
+            <Button size={'icon'} >
+              <ChevronRight />
+            </Button>
           </div>
-        ))}
+        </div>
       </div>
     </div>
   )
