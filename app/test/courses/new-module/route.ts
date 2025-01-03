f1f3bb04b708b3efdd405d5a81c0af0ad81b755c
AkshatJha21 <akshatj2134@gmail.com>
2025-01-03 20:20:30 +0530

    test module POST

--- app/test/courses/new-module/route.ts
+++ app/test/courses/new-module/route.ts
@@ -4,8 +4,49 @@ import { moduleSchema } from "@/helpers/zod";
 
 export async function POST (request: NextRequest) {
     try {
-        
+        const body = await request.json();
+        const parsedBody = moduleSchema.safeParse(body);
+
+        if (!parsedBody.success) {
+            return NextResponse.json({
+                error: "Invalid inputs",
+            }, {
+                status: 400
+            });
+        }
+
+        const { title, courseId, index } = parsedBody.data;
+
+        const course = await db.course.findUnique({
+            where: {
+                id: courseId
+            }
+        });
+
+        if (!course) {
+            return NextResponse.json({
+                error: "No course found",
+            }, {
+                status: 404
+            });
+        }
+
+        const newModule = await db.module.create({
+            data: {
+                courseId,
+                title,
+                index
+            }
+        });
+
+        return NextResponse.json({
+            msg: "Successfully created new module",
+            newModule
+        }, {
+            status: 201
+        });
     } catch (error) {
-        
+        console.error(error);
+        return NextResponse.json({ error: 'Failed to create module' }, { status: 500 });
     }
 }
