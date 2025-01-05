8415917d8680cdc8cb6b586de5b9c0a33201b151
AkshatJha21 <akshatj2134@gmail.com>
2025-01-05 23:56:35 +0530

    course fetch

---
+++ app/api/courses/[courseId]/route.ts
@@ -0,0 +1,48 @@
+import { NextRequest, NextResponse } from "next/server";
+import { db } from "@/lib/db";
+
+export async function GET (req: NextRequest,
+    { params }: { params: { courseId: string } }
+) {
+    try {
+        const { courseId } = params;
+
+        if (!courseId) {
+            return NextResponse.json({
+                error: "Course ID is required"
+            }, {
+                status: 400
+            });
+        }
+
+        const course = await db.course.findUnique({
+            where: {
+                id: courseId
+            },
+            include: {
+                modules: true
+            }
+        });
+
+        if (!course) {
+            return NextResponse.json({
+                error: "Course not found",
+            }, {
+                status: 404
+            });
+        }
+
+        return NextResponse.json({
+            course,
+        }, {
+            status: 201
+        });
+    } catch (error) {
+        console.error("Course GET failed");
+        return NextResponse.json({
+            error
+        }, {
+            status: 500
+        });
+    }
+}
