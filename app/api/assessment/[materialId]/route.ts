cb6ef355ac14f79d30585bf8033c70cb74a9a82a
AkshatJha21 <akshatj2134@gmail.com>
2025-01-08 11:50:36 +0530

    get questions

---
+++ app/api/assessment/[materialId]/route.ts
@@ -0,0 +1,45 @@
+import { db } from "@/lib/db";
+import { NextRequest, NextResponse } from "next/server";
+
+
+export async function GET(request: NextRequest,
+    { params }: { params: { materialId: string }}
+) {
+    try {
+        const { materialId } = params;
+
+        const material = await db.material.findUnique({
+            where: {
+                id: materialId
+            },
+            include: {
+                questions: {
+                    include: {
+                        questions: true
+                    }
+                }
+            }
+        });
+
+        if (!material) {
+            return NextResponse.json({
+                error: "Material not found"
+            }, {
+                status: 404
+            });
+        }
+
+        return NextResponse.json({
+            allQuestions: material.questions
+        }, {
+            status: 201
+        });
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json({
+            error: "Something went wrong"
+        }, {
+            status: 500
+        });
+    }
+}
